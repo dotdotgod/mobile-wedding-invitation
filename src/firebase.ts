8a82f705e449fe1cbda0e7918195039f3afc285a
dotdotgod <vgb3766@gmail.com>
2024-06-25 20:00:25 +0900

    init
    Signed-off-by: dotdotgod <vgb3766@gmail.com>

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,13 @@ import { getDatabase } from 'firebase/database';
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
 // 가이드에 따라 firebase config를 설정합니다.
 const firebaseConfig = {
-
+  apiKey: 'AIzaSyAJu9zYERO5IIWZ8nzLCyUJSFFS56yeCh4',
+  authDomain: 'alpc-wedding-invitation.firebaseapp.com',
+  projectId: 'alpc-wedding-invitation',
+  storageBucket: 'alpc-wedding-invitation.appspot.com',
+  messagingSenderId: '551352453879',
+  appId: '1:551352453879:web:5449a804fad9776851e1f2',
+  measurementId: 'G-V2873NSNZL',
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
